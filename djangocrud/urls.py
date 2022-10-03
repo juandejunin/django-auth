6a6f5600d24192c6d1697d66646fd77b01e56916
juandejunin <juandejunin75@gmail.com>
2022-10-03 17:50:42 +0200

    feat: creando aplicacion tareas

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from tareas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hola/', views.hola)
 ]
