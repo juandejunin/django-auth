ce1846eeebde9907e3ca67042b314a8ea7c529e4
juandejunin <juandejunin75@gmail.com>
2022-10-04 17:28:22 +0200

    feat: manejando errores y mostrando en la pagina

--- tareas/views.py
+++ tareas/views.py
@@ -5,28 +5,32 @@ from django.contrib.auth.models import User
 
 
 # Create your views here.
-def home (request):
-    return render (request, 'home.html')
+def home(request):
+    return render(request, 'home.html')
 
-def signup (request):
+
+def signup(request):
 
     if request.method == 'GET':
-        return render (request, "signup.html", {
-        'form': UserCreationForm 
-    })
-  
+        return render(request, "signup.html", {
+            'form': UserCreationForm
+        })
+
     else:
-        if request.POST ['password1'] == request.POST['password2']:
-            #register user
+        if request.POST['password1'] == request.POST['password2']:
+            # register user
             try:
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 return HttpResponse('Usuario creado satisfactoriamente')
             except:
-                return HttpResponse('El usuario ya existe')
-                 
-                 
-        return HttpResponse('las contraseñas no coinciden')
+                return render(request, "signup.html", {
+                    'form': UserCreationForm,
+                    "error": 'El ususrio ya existe'
+                })
 
-
-
+        return render(request, "signup.html", {
+            'form': UserCreationForm,
+            'error': "Las contraseñas no coinciden"
+        })
