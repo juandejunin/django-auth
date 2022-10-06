985b875f86027c38c85dd13a4f9f96380fa6bc09
juandejunin <juandejunin75@gmail.com>
2022-10-06 19:48:41 +0200

    feat: creando modelo de task

--- tareas/admin.py
+++ tareas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
+
+
+#la siguiente clase nos permite visualizar la fecha de creacion en el panel
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ('create', )
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
--- tareas/models.py
+++ tareas/models.py
@@ -1,3 +1,21 @@
+# from email.policy import default
+# from multiprocessing.spawn import import_main_path
+# from ssl import create_default_context
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    create = models.DateField(auto_now_add=True)
+    dateCompleted = models.DateField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#usuario juanci contraseña 1234asdf
+
+#cuando se utilice el modelo voy a utilizar un metodo self, esto nos permite elegir como se define la tarea en la vista del panel de administracion
+
+    def __str__(self):
+        return self.title + ' - by '+ self.user.username
