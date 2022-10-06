b52e233754129532804f702c287bf869071f0f40
juandejunin <juandejunin75@gmail.com>
2022-10-06 08:33:59 +0200

    feat: usando templates y condicionales para crear nav

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -19,6 +19,6 @@ from tareas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('signup/',views.signup, name= 'home'),
+    path('signup/',views.signup, name= 'signup'),
     path('tasks/', views.tasks, name= 'tasks')
 ]
